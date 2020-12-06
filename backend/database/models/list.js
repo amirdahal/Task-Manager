76f1f4c0a60f4f97e1fb4a3b6c27b0bb9383bb07
Amir Dahal <68531462+amirdahal@users.noreply.github.com>
2020-12-06 16:11:29 +0530

    Add files via upload

---
+++ backend/database/models/list.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const listSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        minLength: 3
+    }
+})
+
+const List = mongoose.model('List', listSchema);
+module.exports = List;
